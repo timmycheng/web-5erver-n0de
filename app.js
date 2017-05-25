051b461b4e3114da45876f77942b5ec02441cb79
timmycheng <timmycheng@foxmail.com>
2017-05-25 14:55:07 +0800

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+
+// 打印请求地址
+app.use(async(ctx, next) => {
+    console.log(`${ctx.method} ${ctx.url}`);
+    await next();
+});
+
+// 打印时间
+app.use(async(ctx, next) => {
+    const start = new Date().getTime();
+    await next();
+    const ms = new Date().getTime()-start;
+    console.log(`Time: ${ms}ms`);
+});
+
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async(ctx, next) => {
+    await next();
+    // 设置response的Content-Type
+    ctx.response.type = 'text/html';
+    // 设置response的内容
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+// 在端口3000监听:
+app.listen(3000);
+// 打印信息
+console.log('app started at port 3000...');
